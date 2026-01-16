d085c5a407d7970cf2d5ccc7519c5da268a86e59
CostaDenis <deniscostaf@gmail.com>
2026-01-16 09:31:22 -0300

    feat: persistindo tema

--- Services/ThemeService.cs
+++ Services/ThemeService.cs
@@ -1,14 +1,19 @@
 
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
 namespace portfolio_v2.Services;
 
-public class ThemeService
+public class ThemeService(IJSRuntime jsRuntime)
 {
+    private readonly IJSRuntime _jsRuntime = jsRuntime;
     public string CurrentTheme { get; private set; } = "darkVS";
     public event Action? OnChange;
 
-    public void SetTheme(string theme)
+    public async Task SetTheme(string theme)
     {
         CurrentTheme = theme;
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "theme", theme);
         OnChange?.Invoke();
     }
 }
