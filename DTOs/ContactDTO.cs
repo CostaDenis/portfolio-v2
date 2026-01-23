e56a1c12707fcb0b465d523347bf7fe218ba3406
CostaDenis <deniscostaf@gmail.com>
2026-01-23 12:44:14 -0300

    refactor: carregando dados de contatos via json

---
+++ DTOs/ContactDTO.cs
@@ -0,0 +1,8 @@
+namespace portfolio_v2.DTOs;
+
+public class ContactDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Url { get; set; }
+    public string Value { get; set; } = string.Empty;
+}
